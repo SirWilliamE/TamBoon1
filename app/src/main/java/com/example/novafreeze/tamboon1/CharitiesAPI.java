dc098483c70ac7ef6d9cb24a208ba3781064860b
William C <William@Colachicco.com>
2019-06-12 14:49:28 -0400

    Created Donation class and started post request code.

--- app/src/main/java/com/example/novafreeze/tamboon1/CharitiesAPI.java
+++ app/src/main/java/com/example/novafreeze/tamboon1/CharitiesAPI.java
@@ -5,12 +5,17 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface CharitiesAPI {
 
     @GET("charities")
     Call<Getting> getCharities();
+
+    @POST("donations")
+    Call<Donation> makeDonation(@Body Donation donation);
 
 }
---
+++ app/src/main/java/com/example/novafreeze/tamboon1/Donation.java
@@ -0,0 +1,7 @@
+package com.example.novafreeze.tamboon1;
+
+public class Donation {
+
+    
+
+}
