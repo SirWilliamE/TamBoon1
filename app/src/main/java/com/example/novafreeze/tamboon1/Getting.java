b6de3a7acad902eca940f566f581d651eedf9c92
William C <William@Colachicco.com>
2019-06-12 02:06:46 -0400

    Initial commit. Added gradle dependencies. Set up variables and getters for JSON data.

---
+++ app/src/main/java/com/example/novafreeze/tamboon1/Getting.java
@@ -0,0 +1,22 @@
+package com.example.novafreeze.tamboon1;
+
+public class Getting {
+
+    private int id;
+
+    private String name;
+
+    private String logo_url;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogo_url() {
+        return logo_url;
+    }
+}
